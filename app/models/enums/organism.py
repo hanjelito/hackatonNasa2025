b4f7518ae65848374865dd52c3e7d1da4f8fd46f
Néstor Villa <nestorvilla@patitasco.com>
2025-10-04 15:03:36 +0200

    feat: enums for LLM extraction and filter

---
+++ app/models/enums/organism.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class Organism(Enum):
+    RAT = "RAT"
+    MOUSE = "MOUSE"
+    HUMAN = "HUMAN"
+    HUMAN_CELLS = "HUMAN_CELLS"
+    PLANT = "PLANT"
+    INSECT = "INSECT"
+    NEMATODE = "NEMATODE"
+    MICROORGANISM = "MICROORGANISM"
+    OTHER = "OTHER"
