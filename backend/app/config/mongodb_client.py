73908fbf34226e6382abc0a916dfde4c7e914ab5
Néstor Villa <nestorvilla@patitasco.com>
2025-10-04 16:43:16 +0200

    refactor: update MongoDbClient to use AsyncMongoClient and simplify model handling; adjust FastAPI lifespan management

--- backend/app/config/mongodb_client.py
+++ backend/app/config/mongodb_client.py
@@ -1,21 +1,19 @@
+from typing import Type
+from pymongo import AsyncMongoClient
 from beanie import init_beanie, Document
-from motor.motor_asyncio import AsyncIOMotorClient
-from typing import List, Optional, Type
+from app.models.paper import Paper
 
 
 class MongoDbClient:
-    def __init__(self, url: str, database_name: str = "hackaton_nasa_db", models: Optional[List[Type[Document]]] = None):
-        # Lista de modelos Beanie (Document). Puede pasarse desde fuera.
-        self.models: List[Type[Document]] = models or []
-        self.client = AsyncIOMotorClient(url)
+    async def __init__(self, url: str, database_name: str = "hackaton_nasa_db"):
+        self.models: list[Type[Document]] = [Paper]
+        self.client = AsyncMongoClient(url)
         self.database_name = database_name
 
     async def init(self):
-        # Inicializa Beanie con la base de datos y los modelos proporcionados
         await init_beanie(
             database=self.client[self.database_name], document_models=self.models
         )
 
     async def close(self):
-        # Cierra el cliente de Mongo
-        self.client.close()
+        await self.client.close()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.config.mongodb_client import MongoDbClient
+from app.config.settings import settings
 from routes.paper_route import paper_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    database = MongoDbClient(settings.MONGODB_URL)
+    await database.init()
+    yield
+    await database.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(paper_router)
 # app.include_router(event_router)
