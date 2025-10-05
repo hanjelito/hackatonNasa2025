1cd0a17bec5988c7bc5847758d10d53950975c41
Angel Gonzales <jgonzales@peruyoung.com>
2025-10-05 15:22:57 +0200

    url api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://backend:8000',
+        // target: 'http://backend:8000',
+        target: 'https://vd3ujv7kw1.execute-api.eu-north-1.amazonaws.com',
         changeOrigin: true,
         rewrite: (path: string) => path.replace(/^\/api/, '')
       }
